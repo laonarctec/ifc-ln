791dfc15b9b4ccd4704af594f7ea63569fe0eb6a
1ncarnati0n <pattern.langue@gmail.com>
2026-03-15 14:13:40 +0900

    update in-progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ViewerLayout } from '@/components/viewer/ViewerLayout';
+import { ViewerLayout } from "@/components/viewer/ViewerLayout";
 
 export default function App() {
   return <ViewerLayout />;
